ffdf935b3041401b18e130e08bcc46959264e387
Sarthak0006 <sarthakdhiman0006@gmail.com>
2024-09-14 19:39:04 +0530

    testing Deployment of Frontend.

---
+++ frontend/src/Components/Header/index.tsx
@@ -0,0 +1,16 @@
+import style from './style.module.css'
+
+// export interface IAppProps {
+// }
+
+// export default function App (props: IAppProps) {
+
+
+
+export default function Index () {
+  return (
+    <div className={style.heading}>
+      <h1>Testing Deployment</h1>
+    </div>
+  );
+}
